6ce1148d52112851fa1d29dd857a958c7abb6cbe
shiyu <yu.shi@waylens.com>
2019-06-18 19:04:35 +0800

    commit

---
+++ genosha.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+)
+
+func main() {
+  router := gin.Default()
+  router.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  // However, this one will match /user/john/ and also /user/john/send
+  // If no other routers match /user/john, it will redirect to /user/john/
+  router.GET("/user/:name/*action", func(c *gin.Context) {
+    name := c.Param("name")
+    action := c.Param("action")
+    message := name + " is " + action
+    c.String(http.StatusOK, message)
+  })
+
+  // For each matched request Context will hold the route definition
+  router.POST("/user/:name/*action", func(c *gin.Context) {
+    c.FullPath() == "/user/:name/*action" // true
+  })
+  router.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module genosha
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
+	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980 // indirect
+	golang.org/x/sys v0.0.0-20190616124812-15dcb6c0061f // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190614205625-5aca471b1d59 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
