0a07295bf099d16be537c65160fb7990ec06e960
marvinysh <marvinysh@outlook.com>
2019-06-19 09:16:06 +0800

    modify

--- genosha.go
+++ genosha.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
+  "log"
   "net/http"
 )
 
@@ -17,13 +18,23 @@ func main() {
   router.GET("/user/:name/*action", func(c *gin.Context) {
     name := c.Param("name")
     action := c.Param("action")
+    //_ := c.DefaultQuery("firstname", "Guest")
+    //_ = c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
     message := name + " is " + action
     c.String(http.StatusOK, message)
+    aaa(message)
+    log.Println("bbb", message)
   })
 
   // For each matched request Context will hold the route definition
   router.POST("/user/:name/*action", func(c *gin.Context) {
-    c.FullPath() == "/user/:name/*action" // true
+    //c.FullPath() == "/user/:name/*action" // true
   })
   router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func aaa(message string)  {
+  message = message + message
+  log.Println("aaa", message)
 }
