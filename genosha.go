f16eb8684b4b0234590529fa93c3b9b1dfd3e54c
shiyu <yu.shi@waylens.com>
2019-06-20 19:02:39 +0800

    update

--- genosha.go
+++ genosha.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "github.com/gin-gonic/gin"
   "log"
   "net/http"
@@ -31,6 +32,29 @@ func main() {
   router.POST("/user/:name/*action", func(c *gin.Context) {
     //c.FullPath() == "/user/:name/*action" // true
   })
+
+  router.POST("/form_post", func(c *gin.Context) {
+    message := c.PostForm("message")
+    nick := c.DefaultPostForm("nick", "anonymous")
+
+    c.JSON(200, gin.H{
+      "status":  "posted",
+      "message": message,
+      "nick":    nick,
+    })
+  })
+
+  router.POST("/upload", func(c *gin.Context) {
+    // single file
+    file, _ := c.FormFile("file")
+    log.Println(file.Filename)
+
+    // Upload the file to specific dst.
+    c.SaveUploadedFile(file, dst)
+
+    c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+  })
+
   router.Run() // listen and serve on 0.0.0.0:8080
 }
 
